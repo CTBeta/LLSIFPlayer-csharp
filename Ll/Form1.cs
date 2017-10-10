28859af457c155390b37a93ccff2f62e1c972f11
bdbai <bdbaiapp@163.com>
2017-10-10 14:21:40 +0800

    添加项目文件。

---
+++ Ll/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using Dapplo.Windows.Input;
+using Dapplo.Windows.Input.Enums;
+
+namespace Ll
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void WriteLine(string str)
+        {
+            BeginInvoke(new Action(() => button1.Text = str));
+        }
+
+        private void hookKeyboard()
+        {
+            KeyboardHook.KeyboardEvents.Subscribe(e =>
+            {
+                switch (e.Key)
+                {
+                    case VirtualKeyCodes.KEY_D:
+                        grandOffset += 0.005;
+                        Debug.WriteLine($"offset up to {grandOffset}");
+                        break;
+                    case VirtualKeyCodes.KEY_A:
+                        grandOffset -= 0.005;
+                        Debug.WriteLine($"offset down to {grandOffset}");
+                        break;
+                    case VirtualKeyCodes.KEY_W:
+                        amp += 0.005;
+                        Debug.WriteLine($"amp up to {amp}");
+                        break;
+                    case VirtualKeyCodes.KEY_S:
+                        amp -= 0.005;
+                        Debug.WriteLine($"amp down to {amp}");
+                        break;
+                }
+            });
+            Debug.WriteLine("Keyboard hooked");
+        }
+
+        private void prepare()
+        {
+            WriteLine("3");
+            Thread.Sleep(1000);
+            WriteLine("2");
+            Thread.Sleep(1000);
+            WriteLine("1");
+            Thread.Sleep(1000);
+            InputGenerator.KeyPress(VirtualKeyCodes.KEY_K);
+            Thread.Sleep(233);
+
+        }
+
+        private double grandOffset = 0.0;
+        private double amp = 1.0;
+
+        /// <summary>
+        /// Wait strategy
+        /// </summary>
+        /// <param name="_keys"></param>
+        private void waitRunner(object _keys)
+        {
+            SortedSet<KeyAction> keys = (SortedSet<KeyAction>)_keys;
+            prepare();
+            DateTime initTime = DateTime.Now;
+            double lastStop = 0.0;
+
+            foreach (var item in keys)
+            {
+                double offset = item.Offset;
+                if (lastStop == 0.0)
+                {
+                    lastStop = offset;
+                }
+
+                int waitMs = (int)((offset - lastStop) * 1000 * amp);
+                Debug.WriteLine("Waiting for ms:" + waitMs.ToString());
+                Thread.Sleep(waitMs);
+                lastStop = offset;
+
+                item.SendInput();
+        
+            }
+            Environment.Exit(0);
+        }
+
+        /// <summary>
+        /// Loop strategy
+        /// </summary>
+        /// <param name="_keys"></param>
+        private void loopRunner(object _keys)
+        {
+            SortedSet<KeyAction> keys = (SortedSet<KeyAction>)_keys;
+            prepare();
+            DateTime initTime = DateTime.Now;
+            DateTime nowTime;
+            double initOffset = keys.First().Offset;
+
+            foreach (var item in keys)
+            {
+                nowTime = DateTime.Now;
+                while ((nowTime - initTime).TotalMilliseconds < (item.Offset - initOffset - grandOffset) * 1000 * amp)
+                {
+                    Thread.Sleep(1);
+                    nowTime = DateTime.Now;
+                }
+
+                item.SendInput();
+
+            }
+
+            Environment.Exit(0);
+        }
+
+        Thread t;
+
+        private void start(Action<object> strategy)
+        {
+            var keys = KeyAction.ParseJson("./test.json");
+            t = new Thread(new ParameterizedThreadStart(strategy));
+            t.Start(keys);
+            hookKeyboard();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            start(waitRunner);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (t != null)
+            {
+                t.Abort();
+            }
+            Environment.Exit(0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBox1.Text = grandOffset.ToString();
+            textBox2.Text = amp.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            start(loopRunner);
+        }
+    }
+}
---
+++ Ll/KeyAction.cs
@@ -0,0 +1,86 @@
+﻿using Dapplo.Windows.Input;
+using Dapplo.Windows.Input.Enums;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ll
+{
+    class KeyAction: IComparable<KeyAction>
+    {
+        public int Num;
+        public KeyType Type;
+        public double Offset;
+
+        public int CompareTo(KeyAction other)
+        {
+            return Offset - other.Offset > 0 ? 1 : -1;
+        }
+
+        public static SortedSet<KeyAction> ParseJson(string path)
+        {
+            SortedSet<KeyAction> keys = new SortedSet<KeyAction>();
+            using (StreamReader r = new StreamReader("./test.json"))
+            {
+                dynamic obj = JsonConvert.DeserializeObject(r.ReadToEnd());
+                dynamic res = obj.response_data;
+                foreach (var n in res.live_info[0].notes_list)
+                {
+                    switch ((int)n.effect.Value)
+                    {
+                        case 1:
+                        case 2:
+                        case 4:
+                            keys.Add(new KeyAction()
+                            {
+                                Num = n.position,
+                                Type = KeyType.KeyPress,
+                                Offset = n.timing_sec
+                            });
+                            break;
+                        case 3:
+                            keys.Add(new KeyAction()
+                            {
+                                Num = n.position,
+                                Type = KeyType.KeyDown,
+                                Offset = n.timing_sec
+                            });
+                            keys.Add(new KeyAction()
+                            {
+                                Num = n.position,
+                                Type = KeyType.KeyUp,
+                                Offset = n.timing_sec + (double)n.effect_value
+                            });
+                            break;
+                    }
+                }
+            }
+            return keys;
+        }
+
+        public void SendInput()
+        {
+            switch (Type)
+            {
+                case KeyType.KeyDown:
+                    InputGenerator.KeyDown((VirtualKeyCodes)(Num + 48));
+                    Debug.WriteLine("Pushed" + Num.ToString());
+                    break;
+                case KeyType.KeyUp:
+                    InputGenerator.KeyUp((VirtualKeyCodes)(Num + 48));
+                    Debug.WriteLine("Released " + Num.ToString());
+                    break;
+                case KeyType.KeyPress:
+                    InputGenerator.KeyPress((VirtualKeyCodes)(Num + 48));
+                    Debug.WriteLine("Pressed" + Num.ToString());
+                    break;
+            }
+
+        }
+    }
+}
